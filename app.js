56120021b27b4ce4b6462ade8bac2a34566e6dbc
Tom Booth <tombeezle@gmail.com>
2022-09-13 16:13:18 -0700

    tested event listeners with console log

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 /* Imports */
 
 /* State */
-let gameState = 'results'; // 'results' or 'choose'
-let choose = ''; // 'rock' 'paper' 'scissors'
+let gameState = 'results'; // 'results' or 'choice'
+let choice = ''; // 'rock' 'paper' 'scissors'
 let results = ''; // 'win' 'lose' 'draw'
+let computer = 'scissors'; // // 'rock' 'paper' 'scissors'
 
 /* Actions */
 function loadPage() {}
+
+function playAgain() {
+    gameState = '';
+}
 
 /* Components */
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
-const playAgain = document.getElementById('play-again');
+// const playAgain = document.getElementById('play-again');
 
 /* Component */
 // get DOM
 
 // display
 // event listeners
+rock.addEventListener('click', () => {
+    choice = 'rock';
+    gameState = 'results';
+    // console.log('rock pick');
+});
+
+paper.addEventListener('click', () => {
+    choice = 'paper';
+    gameState = 'results';
+    // console.log('paper pick');
+});
+
+scissors.addEventListener('click', () => {
+    choice = 'scissors';
+    gameState = 'results';
+    // console.log('scissors pick');
+});
 
 /* Run page load code */
 loadPage();
