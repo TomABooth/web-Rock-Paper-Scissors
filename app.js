a503812f33a4cf035c01035470a3edbf413ff3a3
Tom Booth <tombeezle@gmail.com>
2022-09-13 15:56:52 -0700

    added state and last commit was adding components not event listeners

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 /* Imports */
 
 /* State */
+let gameState = 'results'; // 'results' or 'choose'
+let choose = ''; // 'rock' 'paper' 'scissors'
+let results = ''; // 'win' 'lose' 'draw'
 
 /* Actions */
 function loadPage() {}
 
 /* Components */
-
-/* Component */
-// get DOM
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const playAgain = document.getElementById('play-again');
+
+/* Component */
+// get DOM
 
 // display
 // event listeners
