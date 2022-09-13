b18f92813276cc4f3ea10f699877a4483beceb50
Tom Booth <tombeezle@gmail.com>
2022-09-13 15:32:45 -0700

    added event listeners and finished up css and html

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ function loadPage() {}
 
 /* Component */
 // get DOM
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const playAgain = document.getElementById('play-again');
+
 // display
 // event listeners
 
