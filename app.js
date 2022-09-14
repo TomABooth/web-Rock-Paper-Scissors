8d7c6baae50ee2a34e800cab2557060d483dd964
Tom Booth <tombeezle@gmail.com>
2022-09-13 17:33:21 -0700

    added constants for scoreboard display, broke html display of scoreboard data

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 /* Imports */
 
 /* State */
-let gameState = 'results'; // 'results' or 'choice'
+let gameState = ''; // 'results' or 'choice'
 let choice = ''; // 'rock' 'paper' 'scissors'
 let results = ''; // 'win' 'lose' 'draw'
 let computer = 'scissors'; // // 'rock' 'paper' 'scissors'
+
+let wins = 2;
+let total = 0;
 
 /* Actions */
 function loadPage() {}
@@ -15,8 +18,18 @@ const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const playAgain = document.getElementById('play-again');
 
+const winscore = document.getElementById('win-score');
+const losescore = document.getElementById('lose-score');
+const drawscore = document.getElementById('draw-score');
+
 /* Component */
 // get DOM
+if (gameState === 'choice') {
+    let wins = 0;
+    let total = 0;
+    let draw = total - wins;
+    console.log(choice);
+}
 
 // display
 // event listeners
